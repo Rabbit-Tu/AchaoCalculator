23dda22e83d0dca449779bdec0b16e4ee0a3ef60
Rabbit-Tu <794401381@qq.com>
2019-09-20 15:49:45 +0800

    Rabbit-Tu

---
+++ Rabbit-Tu/Calcilator/Calcilator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcilator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("请输入要出题的个数：");
+                int n = Convert.ToInt32(Console.ReadLine());
+                opreation.PrintCalculator(n);
+                Console.ReadKey();
+        }
+    }
+}
---
+++ Rabbit-Tu/Calcilator/Calcilator/opreation.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcilator
+{
+    class opreation
+    {
+        public static void PrintCalculator(int n)
+        {
+            Random rd = new Random();
+            int a, b, c, d, e, r;
+            int i = 0;
+            string str;
+            do
+            {
+                a = rd.Next(0, 100);
+                b = rd.Next(0, 100);
+                c = rd.Next(0, 100);
+                d = rd.Next(0, 100);
+                e = rd.Next(1, 100);
+
+                string t = Convert.ToString(rd.Next(7));
+                switch (t)
+                {
+                    case "0":
+                        r = a + b - c;
+                        str = a + " + " + b + " - " + c + " = " + r;
+                        if (r >= 0)
+                        {
+                            Console.WriteLine(str);
+
+                            i++;
+                        }
+
+                        break;
+                    case "1":
+                        r = a - b + c;
+                        str = a + " - " + b + " + " + c + " = " + r;
+                        if (r >= 0)
+                        {
+                            Console.WriteLine(str);
+                            i++;
+                        }
+
+                        break;
+                    case "2":
+                        r = a / e - c;
+                        str = a + " / " + e + " - " + c + " = " + r;
+                        if (a % e == 0 && r >= 0)
+                        {
+                            Console.WriteLine(str);
+
+                            i++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        break;
+                    case "3":
+                        r = a / e * c;
+                        str = a + " / " + e + " * " + c + " = " + r;
+                        if (a % e == 0 && r >= 0)
+                        {
+                            Console.WriteLine(str);
+                            i++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        break;
+                    case "4":
+                        r = a * b - c + d;
+                        str = a + " * " + b + " - " + c + " + " + d + " = " + r;
+                        if (r >= 0)
+                        {
+                            Console.WriteLine(str);
+                            i++;
+                        }
+
+                        break;
+                    case "5":
+                        r = a * b - c / e;
+                        str = a + " * " + b + " - " + c + " / " + e + " = " + r;
+                        if (c % e == 0 && r >= 0)
+                        {
+                            Console.WriteLine(str);
+                            i++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        break;
+                    case "6":
+                        r = a * b * c + d;
+                        str = a + " * " + b + " * " + c + " + " + d + " = " + r;
+                        if (r >= 0)
+                        {
+                            Console.WriteLine(str);
+                            i++;
+                        }
+
+                        break;
+                }
+            } while (i < n);
+        }
+    }
+}
